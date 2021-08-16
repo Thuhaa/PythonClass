3585d3ab28f3f18cb3a32245e4f211b82f448abd
Thuhaa <josephthuha@gmail.com>
2021-08-16 19:54:52 +0300

    Reconfigured the directory

--- modules_python.py
+++
@@ -1,9 +0,0 @@
-# Python's module
-# Spelling
-# Inputs
-# == != is,
-import random
-import string
-# Concatenating Strings
-#ASCII = American Standard Code for Information Interchage
-print("The Random Value I have selected is: ", random.choice(string.ascii_letters))
